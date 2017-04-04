ef2687da59853e8ecb12bceb6d50dc7c46d793c8
Andrii Postoliuk <andriipostoliuk@MacBook-Air-Andrii.local>
2017-04-04 12:11:17 +0300

    actual code

--- calculator/script.js
+++ calculator/script.js
@@ -1,9 +1,29 @@
-function twoo() { 
-    document.getElementById("kkk").value = 2;   
-}
-function three() { 
-    document.getElementById("kkk").value = 3; 
-} 
+'use strict'
+let one = () => document.getElementById("mainForm").value += 1,
+    twoo = () => document.getElementById("mainForm").value += 2,
+    three = () => document.getElementById("mainForm").value += 3,
+    four = () => document.getElementById("mainForm").value += 4,
+    five = () => document.getElementById("mainForm").value += 5,
+    six = () => document.getElementById("mainForm").value += 6,
+    seven = () => document.getElementById("mainForm").value += 7,
+    eight = () => document.getElementById("mainForm").value += 8,
+    nine = () => document.getElementById("mainForm").value += 9,
+    zero = () => document.getElementById("mainForm").value += 0,
+    result = () => mainFun(),
+    
+                        /*operatoin*/
+    
+    minus = () => document.getElementById("mainForm").value += '-',
+    plus = () => document.getElementById("mainForm").value += '+',
+    multiplication = () => document.getElementById("mainForm").value += '*',
+    divizion = () => document.getElementById("mainForm").value += '/',
+    clearing = () => document.getElementById("mainForm").value += ' ';
+    
+                        /*main Math Function*/
+let mainFun = () => console.log( document.getElementById("mainForm").value );
+
+
+
 
 
 
