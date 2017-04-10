cdeb2b8899336788c848b5730a0d4d064e64e166
Andrii Postoliuk <andriipostoliuk@MacBook-Air-Andrii.local>
2017-04-10 14:49:05 +0300

    add moda Function

--- Varial/script.js
+++ Varial/script.js
@@ -1,19 +1,34 @@
 'use strict'
 
-const mainArray = [4, 4, 3, 2, 2, 2, 6, 6, 7, 3];
+const mainArray = [4, 4, 3, 2, 2, 2, 6, 6, 2, 2, 2, 7, 3];
 const varialLine = mainArray => console.log( 'варіаційний ряд ' + mainArray.sort() ); varialLine(mainArray);
 const countEl = mainArray => console.log( 'розмах ' + mainArray.length ); countEl(mainArray);
 const mediana = () => console.log('медіана ' + mainArray.sort()[Math.floor(mainArray.length/2)]); mediana();
 const vibMedium = () => console.log('середнє значення ' + mainArray.reduce((sum, current) => sum + current)/mainArray.length); vibMedium();
-(function staticF() {
+function staticF() {
   const count = {}; 
-  mainArray.forEach(function(i) { count[i] = (count[i] || 0) + 1; });
-  console.log(count);
-}());
+  return ( function() {
+    mainArray.forEach(function(i) { count[i] = (count[i] || 0) + 1; });
+    console.log(count);
+    let minKey = Object.keys(count)[0];
+    Object.keys(count).forEach(function(key) {
+        if(minKey < key) { 
+           minKey = key;
+        }
+    });
+////
+    
+    for(let i = 0; i < Object.keys(count).length; i++) {
+      let minVal = Object.keys(count)[0];
+      let valСurrent = Object.keys(count)[i];
+      if(count[minVal] < count[valСurrent]) {
+        minVal = valСurrent;
+      }
+      console.log(count[ minVal ]);
+    }  
+  }());
+}staticF();
 
-  
-
- 
 
                     
                     
